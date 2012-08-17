fecd34aee7a7d93ee9438963117b16e257a25ac6
ap4y <lod@pisem.net>
2012-08-17 21:31:10 +0400

    album cell implemented

--- Origami/Views/ORGMAlbumCell.h
+++ Origami/Views/ORGMAlbumCell.h
@@ -9,5 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface ORGMAlbumCell : UITableViewCell
-
+- (void)setAlbums:(NSArray *)albums;
 @end
--- Origami/Views/ORGMAlbumCell.m
+++ Origami/Views/ORGMAlbumCell.m
@@ -8,22 +8,62 @@
 
 #import "ORGMAlbumCell.h"
 
+#import "ORGMAlbum.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface ORGMAlbumCell ()
+@property (weak, nonatomic) IBOutlet UIView *leftAlbumView;
+@property (weak, nonatomic) IBOutlet UIView *rigthAlbumView;
+@property (weak, nonatomic) IBOutlet UIImageView *leftImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *rightImageView;
+@property (weak, nonatomic) IBOutlet UILabel *leftTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rightTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *leftDetailsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rightDetailsLabel;
+
+@end
+
 @implementation ORGMAlbumCell
+@synthesize leftAlbumView;
+@synthesize rigthAlbumView;
+@synthesize leftImageView;
+@synthesize rightImageView;
+@synthesize leftTitleLabel;
+@synthesize rightTitleLabel;
+@synthesize leftDetailsLabel;
+@synthesize rightDetailsLabel;
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+- (void)setAlbums:(NSArray *)albums {
+    [leftAlbumView setHidden:YES];
+    [rigthAlbumView setHidden:YES];
+    
+    if (albums.count > 0) {
+        if ([[albums objectAtIndex:0] isKindOfClass:[ORGMAlbum class]]) {
+            [leftAlbumView setHidden:NO];
+            ORGMAlbum *firstAlbum = [albums objectAtIndex:0];
+            leftTitleLabel.text = firstAlbum.title;
+            leftDetailsLabel.text = firstAlbum.album_artist;
+            if (![firstAlbum.random_cover isEqualToString:kMissingImageUrl]) {
+                [leftImageView setImageWithURL:[NSURL URLWithString:firstAlbum.random_cover]];
+            } else {
+                [leftImageView setImage:nil];
+            }
+        }
     }
-    return self;
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+    
+    if (albums.count > 1) {
+        if ([[albums objectAtIndex:1] isKindOfClass:[ORGMAlbum class]]) {
+            [rigthAlbumView setHidden:NO];
+            ORGMAlbum *secondAlbum = [albums objectAtIndex:1];
+            rightTitleLabel.text = secondAlbum.title;
+            rightDetailsLabel.text = secondAlbum.album_artist;
+            if (![secondAlbum.random_cover isEqualToString:kMissingImageUrl]) {
+                [rightImageView setImageWithURL:[NSURL URLWithString:secondAlbum.random_cover]];
+            } else {
+                [rightImageView setImage:nil];
+            }
+        }
+    }
 }
 
 @end
