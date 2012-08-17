8fcf1902e836e1d5db86e76c6c18d4dfa2c08fda
ap4y <lod@pisem.net>
2012-08-17 21:29:47 +0400

    album cell added

---
+++ Origami/Views/ORGMAlbumCell.h
@@ -0,0 +1,13 @@
+//
+//  ORGMAlbumCell.h
+//  Origami
+//
+//  Created by ap4y on 8/17/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ORGMAlbumCell : UITableViewCell
+
+@end
---
+++ Origami/Views/ORGMAlbumCell.m
@@ -0,0 +1,29 @@
+//
+//  ORGMAlbumCell.m
+//  Origami
+//
+//  Created by ap4y on 8/17/12.
+//
+//
+
+#import "ORGMAlbumCell.h"
+
+@implementation ORGMAlbumCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
