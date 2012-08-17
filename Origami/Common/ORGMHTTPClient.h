b346d67a4959b2564d331083387061d955b9caf4
ap4y <lod@pisem.net>
2012-08-17 21:30:05 +0400

    missing cover image url exposed

--- Origami/Common/ORGMHTTPClient.h
+++ Origami/Common/ORGMHTTPClient.h
@@ -9,6 +9,7 @@
 #import "AFHTTPClient.h"
 
 #define kTTBaseURLString @"http://loon.herokuapp.com/api/v0/"
+#define kMissingImageUrl @"/cover_arts/normal/missing.png"
 
 @interface ORGMHTTPClient : AFHTTPClient
 + (ORGMHTTPClient*)sharedClient;
