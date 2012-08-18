6845145ef4903d301d59fa999d69be92f522206a
ap4y <lod@pisem.net>
2012-08-18 13:49:18 +0400

    added lastfm covers to the album cell

--- Origami/Views/ORGMAlbumCell.m
+++ Origami/Views/ORGMAlbumCell.m
@@ -10,6 +10,7 @@
 
 #import "ORGMAlbum.h"
 #import "UIImageView+AFNetworking.h"
+#import "ORGMLastfmProxyClient.h"
 
 @interface ORGMAlbumCell ()
 @property (weak, nonatomic) IBOutlet UIView *leftAlbumView;
@@ -20,6 +21,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *rightTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *leftDetailsLabel;
 @property (weak, nonatomic) IBOutlet UILabel *rightDetailsLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *leftLastfmLogo;
+@property (weak, nonatomic) IBOutlet UIImageView *rightLastfmLogo;
 
 @end
 
@@ -32,6 +35,8 @@
 @synthesize rightTitleLabel;
 @synthesize leftDetailsLabel;
 @synthesize rightDetailsLabel;
+@synthesize leftLastfmLogo;
+@synthesize rightLastfmLogo;
 
 - (void)setAlbums:(NSArray *)albums {
     [leftAlbumView setHidden:YES];
@@ -40,13 +45,23 @@
     if (albums.count > 0) {
         if ([[albums objectAtIndex:0] isKindOfClass:[ORGMAlbum class]]) {
             [leftAlbumView setHidden:NO];
+            [leftLastfmLogo setHidden:YES];
             ORGMAlbum *firstAlbum = [albums objectAtIndex:0];
             leftTitleLabel.text = firstAlbum.title;
             leftDetailsLabel.text = firstAlbum.album_artist;
             if (![firstAlbum.random_cover isEqualToString:kMissingImageUrl]) {
                 [leftImageView setImageWithURL:[NSURL URLWithString:firstAlbum.random_cover]];
             } else {
-                [leftImageView setImage:nil];
+                NSURL *imageUrl =
+                    [[ORGMLastfmProxyClient sharedClient] albumImageUrlForArtist:firstAlbum.album_artist
+                                                                      albumTitle:firstAlbum.title];
+                [leftImageView setImageWithURLRequest:[NSURLRequest requestWithURL:imageUrl]
+                                     placeholderImage:nil
+                                              success:^(NSURLRequest *request,
+                                                        NSHTTPURLResponse *response,
+                                                        UIImage *image) {
+                                                  [leftLastfmLogo setHidden:NO];
+                                              } failure:nil];
             }
         }
     }
@@ -54,13 +69,23 @@
     if (albums.count > 1) {
         if ([[albums objectAtIndex:1] isKindOfClass:[ORGMAlbum class]]) {
             [rigthAlbumView setHidden:NO];
+            [rightLastfmLogo setHidden:YES];
             ORGMAlbum *secondAlbum = [albums objectAtIndex:1];
             rightTitleLabel.text = secondAlbum.title;
             rightDetailsLabel.text = secondAlbum.album_artist;
             if (![secondAlbum.random_cover isEqualToString:kMissingImageUrl]) {
                 [rightImageView setImageWithURL:[NSURL URLWithString:secondAlbum.random_cover]];
             } else {
-                [rightImageView setImage:nil];
+                NSURL *imageUrl =
+                    [[ORGMLastfmProxyClient sharedClient] albumImageUrlForArtist:secondAlbum.album_artist
+                                                                      albumTitle:secondAlbum.title];
+                [rightImageView setImageWithURLRequest:[NSURLRequest requestWithURL:imageUrl]
+                                      placeholderImage:nil
+                                               success:^(NSURLRequest *request,
+                                                         NSHTTPURLResponse *response,
+                                                         UIImage *image) {
+                                                   [rightLastfmLogo setHidden:NO];
+                                               } failure:nil];
             }
         }
     }
